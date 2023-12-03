5836e5483ecbc0e635429fd016c51539647e1357
StoryTimeSanta <136290597+StoryTimeSanta@users.noreply.github.com>
2023-12-03 15:16:32 -0600

    Add files via upload

---
+++ poem.py
@@ -0,0 +1,8 @@
+# Richard Hudson
+
+noun = input("Please enter a plural noun: ")
+adj = input("Please enter an adjective: ")
+
+print(noun, "are red, violets are blue")
+# f string is formatted to remove the space
+print(f'Monty Python is {adj}, woo hoo!')
