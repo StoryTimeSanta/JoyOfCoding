c263f5ace35211857cfcd805cb237b20f21f81dd
StoryTimeSanta <136290597+StoryTimeSanta@users.noreply.github.com>
2023-12-03 15:53:15 -0600

    Add files via upload

---
+++ madfun.py
@@ -0,0 +1,11 @@
+# Mad Fun
+# Richard Hudson
+
+import math
+from math import sqrt
+
+# Get a decimal number from the user
+decimal = eval(input("Please enter a number with a decimal: "))
+print(decimal)
+print(round(decimal))
+print(sqrt(decimal))
