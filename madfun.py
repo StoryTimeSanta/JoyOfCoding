18012f8f0094f174b83d9d5fa046def8eeb96611
StoryTimeSanta <136290597+StoryTimeSanta@users.noreply.github.com>
2023-12-03 16:15:53 -0600

    Add files via upload

--- madfun.py
+++ madfun.py
@@ -6,6 +6,6 @@ from math import sqrt
 
 # Get a decimal number from the user
 decimal = eval(input("Please enter a number with a decimal: "))
-print(decimal)
-print(round(decimal))
-print(sqrt(decimal))
+print("Absolute Value:", abs(decimal))
+print("Rounded:", round(decimal, 0))
+print("Square root:", (sqrt(abs(round(decimal)))))
