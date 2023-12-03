d20eef3399c7b6f58e96066fc73234c477c6fc07
StoryTimeSanta <136290597+StoryTimeSanta@users.noreply.github.com>
2023-12-03 15:35:07 -0600

    Add files via upload

--- poem.py
+++ poem.py
@@ -3,6 +3,6 @@
 noun = input("Please enter a plural noun: ")
 adj = input("Please enter an adjective: ")
 
-print(noun, "are red, violets are blue")
+print(noun.capitalize(), "are red, violets are blue")
 # f string is formatted to remove the space
 print(f'Monty Python is {adj}, woo hoo!')
