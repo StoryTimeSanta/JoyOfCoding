9ba8c958eb93f47597b46aa1bd20bc63c2c9bb0b
StoryTimeSanta <136290597+StoryTimeSanta@users.noreply.github.com>
2023-12-03 19:05:50 -0600

    Add files via upload

---
+++ Module3.py
@@ -0,0 +1,13 @@
+# Writing Loop Word Problems
+# Richard Hudson
+
+print("Question 1.")
+for i in range (10, 26, 5):
+    print(i, end=" ")
+print()
+print()
+
+print("Question 2.")
+for i in range (-3, 21, 3):
+    print(i, end=", ")
+print(i + 3)
