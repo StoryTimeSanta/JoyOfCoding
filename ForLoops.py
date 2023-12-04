5b2cdebcd23ccdfe962cb5d1441f3b618f67ee14
StoryTimeSanta <136290597+StoryTimeSanta@users.noreply.github.com>
2023-12-03 18:31:46 -0600

    Add files via upload

---
+++ ForLoops.py
@@ -0,0 +1,32 @@
+# For Loops
+# Richard Hudson
+
+print("Question 1a.")
+for i in range (1, 6, 1):
+    print(i)
+print()
+
+print("Question 1b.")
+for i in range (2, 12, 3):
+    print(i)
+print()
+
+print("Question 1c.")
+for i in range (-10, 2, 2):
+    print(i, end=" ")
+print()
+print()
+
+print("Question 1d.")
+for i in range(4):
+    print("****")
+print()
+
+print("Question 2.")
+for i in "CSCI 150":
+    print(i)
+print()
+
+print("Question 3.")
+for i in range (1, 11, 1):
+    print(i)
