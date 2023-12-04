cc2b2ecc8fe3ce1970ac6840021fd4539aa90ff5
StoryTimeSanta <136290597+StoryTimeSanta@users.noreply.github.com>
2023-12-03 21:34:14 -0600

    Add files via upload

---
+++ if.py
@@ -0,0 +1,26 @@
+# What does this program output when it's ...
+# 1. Monday?
+# 2. Saturday?
+# 3. Sunday?
+# 4. Raining?
+
+# Write what each line should print next to the code above for the 4
+# example inputs on lines 2—5. Consult with a peer before running the
+# example if possible. Were your hypotheses correct? Why or why not?
+
+# saturday - Wake up at 7 am
+# sunday - Wake up at 7 am
+# satURday - Wake up at 7 am
+# SunDAY - Wake up at 7 am
+
+DoW = input("What day is it?")
+DoWT = DoW.title() # This line is the fix for title method
+# DoW = "Monday"
+
+if DoWT == "Saturday": # adjusted for new DoWT variable
+    print("Wake up at 9 am")
+elif DoWT == "Sunday": # adjusted for new DoWT variable
+    print("Wake up at 10 am")
+else:
+    print("Wake up at 7 am")
+
