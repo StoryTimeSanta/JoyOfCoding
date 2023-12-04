adddbcaea69db75c6fbcb50da4c37cca4e51406f
StoryTimeSanta <136290597+StoryTimeSanta@users.noreply.github.com>
2023-12-03 21:57:58 -0600

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,18 @@
+# Learning Excercise: Conditions
+
+x = 3
+y = 7
+
+# Excercise
+print( not x == 5 ) # True
+print ( x > 0 and y > 0 ) # True
+print ( x < 0 or y < 0 ) # False
+print ( x*y > 20 or x*y < -20 ) # True
+
+print()
+
+# Challenge
+print( not (x == 0 or y == 5) ) # True
+print( not x == 0 or y == 5 ) # True
+print( x == 3 and not y == 5 ) # True
+print( not (x == 3 and y == 5) ) # True
